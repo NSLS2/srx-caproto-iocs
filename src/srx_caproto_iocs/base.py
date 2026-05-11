b5246afcc24f3bb25b79df1e40d59aea67eb00f3
Max Rakitin <mrakitin@bnl.gov>
2026-05-11 16:34:27 -0400

    Update pixi config with dev dependencies; update test to use "a" mode for saving hdf5 files

--- src/srx_caproto_iocs/base.py
+++ src/srx_caproto_iocs/base.py
@@ -214,7 +214,7 @@ class CaprotoSaveIOC(PVGroup):
             data = received["data"]
             frame_number = received["frame_number"]
             try:
-                save_hdf5_nd(fname=filename, data=data, mode="x", group_path="enc1")
+                save_hdf5_nd(fname=filename, data=data, mode="a", group_path="enc1")
                 print(
                     f"{now()}: saved {frame_number=} {data.shape} data into:\n  {filename}"
                 )
